ee8cf4c5777a727525d988850bccd4b141c5e602
mohamed saoud <darwynsaoud@gmail.com>
2026-07-20 19:40:02 +0100

    Remove unnecessary BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
